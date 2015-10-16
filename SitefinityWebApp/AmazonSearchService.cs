d510791e7e84a585dd899034c2f3a12000bb2894
bonny-bonev <bonny.bonev@telerik.com>
2015-10-16 16:57:26 +0300

    Task - #172569 - Implement error logging for Amazon search service

--- SitefinityWebApp/AmazonSearchService.cs
+++ SitefinityWebApp/AmazonSearchService.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
-using System.Security;
 using System.Security.Permissions;
 using System.Text;
 using AmazingCloudSearch;
+using AmazingCloudSearch.Contract.Result;
 using Amazon;
 using Amazon.CloudSearch;
 using Amazon.CloudSearch.Model;
 using Amazon.CloudSearchDomain;
 using Amazon.CloudSearchDomain.Model;
 using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Configuration;
 using Telerik.Sitefinity.Services.Search;
+using Telerik.Sitefinity.Services.Search.Configuration;
 using Telerik.Sitefinity.Services.Search.Data;
 using Telerik.Sitefinity.Services.Search.Web.UI.Public;
-using Telerik.Sitefinity.Configuration;
-using Telerik.Sitefinity.Services.Search.Configuration;
-using System.Collections.Specialized;
 
 namespace SitefinityWebApp
 {
@@ -74,8 +74,7 @@ namespace SitefinityWebApp
                         defineRequest.DomainName = name;
                         defineRequest.Suggester = suggester;
 
-                        // You can use the response to handle errors
-                        var responce = cloudSearchClient.DefineSuggester(defineRequest);
+                        cloudSearchClient.DefineSuggester(defineRequest);
                     }
 
                     searchResults.Dispose();
@@ -159,7 +158,11 @@ namespace SitefinityWebApp
         {
             var amazonSearchParameters = this.GetAmazonParams();
             CloudSearch<AmazonDoc> cloudSearch = new CloudSearch<AmazonDoc>(amazonSearchParameters[DocumentEndPoint], amazonSearchParameters[ApiVersion]);
-            cloudSearch.Delete(documents.Select(d => new AmazonDoc(d)).ToList());
+            var result = cloudSearch.Delete(documents.Select(d => new AmazonDoc(d)).ToList());
+            if (result.IsError)
+            {
+                Log.Write(string.Format("Failed to remove documents from search index '{0}'. Errors: {1}", indexName, this.GenerateErrorMessage(result)), ConfigurationPolicy.ErrorLog);
+            }
         }
 
         private string BuildQueryFilter(ISearchFilter filter)
@@ -293,6 +296,10 @@ namespace SitefinityWebApp
 
             // You can use the result to handle errors
             var result = cloudSearch.Update(documents.Select(d => new AmazonDoc(d)).ToList());
+            if (result.IsError)
+            {
+                Log.Write(string.Format("Failed to update search index '{0}'. Errors: {1}", name, this.GenerateErrorMessage(result)), ConfigurationPolicy.ErrorLog);
+            }
         }
 
         private NameValueCollection GetAmazonParams()
@@ -307,6 +314,11 @@ namespace SitefinityWebApp
         private string GetSuggesterName(string fieldName)
         {
             return fieldName.ToLowerInvariant() + "_suggester";
+        }
+
+        private string GenerateErrorMessage(BasicResult result)
+        {
+            return result.errors.Select(e => e.message).Aggregate((i, j) => string.Concat(i, ". ", j));
         }
 
         public const string ServiceName = "AmazonSearchService";
