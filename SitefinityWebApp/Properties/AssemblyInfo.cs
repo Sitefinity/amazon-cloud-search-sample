31d0ed03b1ed31bde2579cc2b8b36d1dcdf5f0b1
Aleksandra Stoycheva <astojceva@yahoo.com>
2016-10-05 12:06:04 +0300

    Upgraded to version 9.2.6200.0

--- SitefinityWebApp/Properties/AssemblyInfo.cs
+++ SitefinityWebApp/Properties/AssemblyInfo.cs
@@ -39,6 +39,8 @@ using System.Runtime.InteropServices;
 
 
 
+
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]
