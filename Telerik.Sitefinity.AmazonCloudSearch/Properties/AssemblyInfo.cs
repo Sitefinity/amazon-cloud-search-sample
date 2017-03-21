9b6e5c79152e4fa7493968cbefa7fd01e806d0d4
Georgi Georgiev <georgii.georgiev@telerik.com>
2017-03-21 11:55:22 +0200

    Update Assembly Copyright

--- Telerik.Sitefinity.AmazonCloudSearch/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.AmazonCloudSearch/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Telerik.Sitefinity.AmazonCloudSearch;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Progress")]
 [assembly: AssemblyProduct("Progress Sitefinity CMS")]
-[assembly: AssemblyCopyright("© 2016 Telerik AD. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2005-2016 Telerik AD. All rights reserved.")]
 [assembly: AssemblyTrademark("Sitefinity")]
 [assembly: AssemblyCulture("")]
 
